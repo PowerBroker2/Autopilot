3d1b45a74383dc0568ab1f6a6eb46eca20e65d06
PB2 <gitstuff2@gmail.com>
2019-11-03 00:52:07 -0400

    Add files via upload

--- Autopilot.cpp
+++ Autopilot.cpp
@@ -1,1 +1,148 @@
 #include "Autopilot.h"
+
+
+
+
+float float_constrian(float input, float min, float max)
+{
+	if (input > max)
+		return max;
+	else if (input < min)
+		return min;
+	else
+		return input;
+}
+
+
+
+
+void pitch_controller::begin(float setpoint_,
+                             int maxRate_up_,
+                             int maxRate_down_,
+                             float kp_,
+                             float ki_,
+                             float kd_,
+                             float roll_comp_,
+                             float sampleRate_)
+{
+	setpoint        = float_constrian(setpoint_, MIN_SETPOINT, MAX_SETPOINT);
+	maxRate_up      = float_constrian(maxRate_up_, MIN_RATE_UP, MAX_RATE_UP);
+	maxRate_down    = float_constrian(maxRate_down_, MIN_RATE_DN, MAX_RATE_DN);
+	samplePeriod_s  = float_constrian((1 / sampleRate_), MIN_T, MAX_T);
+	samplePeriod_ms = 1000 * samplePeriod_s;
+	kp              = float_constrian(find_kp(kp_, ki_, kd_, samplePeriod_s), MIN_P, MAX_P);
+	ki              = float_constrian(find_kp(ki_, samplePeriod_s), MIN_I, MAX_I);
+	kd              = float_constrian(kd_, MIN_D, MAX_D);
+	roll_comp       = float_constrian(roll_comp_, MIN_ROLL_COMP, MAX_ROLL_COMP);
+
+	sampleTimer_current  = millis();
+	sampleTimer_previous = sampleTimer_current;
+}
+
+
+
+
+float pitch_controller::compute(float pitchAngle, // Degrees
+                                float rollAngle,  // Degrees
+                                float airspeed)   // m/s
+{
+	float limited_err;
+	float roll_bias;
+	float biased_err;
+	float pitch_rate; // Degrees per Sec
+	float rate_adj;
+	float scaler_output;
+	float controller_output;
+
+	sampleTimer_current = millis();
+	sample_time_actual = sampleTimer_current - sampleTimer_previous;
+
+	if (sample_time >= samplePeriod_ms)
+	{
+		sampleTimer_previous += samplePeriod_ms;
+		
+		previousError = error;
+		error = setpoint - pitchAngle;
+		summedError += (error - previousError) / 2) * (sample_time_actual / 1000); /////////////////////////// <<<<<<<<<<<<<------------
+
+		limited_err = float_constrian(error * omega(), maxRate_down, maxRate_up);
+		roll_bias   = roll_compensation(rollAngle, airspeed);
+		biased_err  = limited_err + roll_bias;
+		pitch_rate  = (pitchAngle - prevPitchAngle) * (sample_time_actual / 1000); // Degrees per Sec
+		rate_adj    = biased_err + pitch_rate;
+		scaler_output = airspeed_scaler * rate_adj;
+
+		p_val = p_component(biased_err);
+		i_val = i_component(scaler_output);
+		d_val = d_component(scaler_output);
+		
+		controller_output = (p_val + i_val + d_val) * airspeed_scaler;
+
+		status = true;
+		return constrain(controller_output, servoMin, servoMax);
+	}
+
+	status = false;
+	return 0;
+}
+
+
+
+
+float pitch_controller::find_kp(float kp_,
+                                float ki_,
+                                float kd_,
+                                float samplePeriod)
+{
+	return (kp_ - (ki_ * samplePeriod)) * (samplePeriod - kd_);
+}
+
+
+
+
+float pitch_controller::find_ki(float ki_, float samplePeriod)
+{
+	return ki_ * samplePeriod;
+}
+
+
+
+
+float pitch_controller::omega()
+{
+	return 1000 / samplePeriod;
+}
+
+
+
+
+float pitch_controller::roll_compensation(float rollAngle, float airspeed)
+{
+	float rollAngle_rad = (M_PI / 180) * rollAngle;
+
+	return (roll_comp * GRAVITY) / (airspeed * tan(rollAngle_rad) * sin(rollAngle_rad));
+}
+
+
+
+
+float pitch_controller::p_component(float input)
+{
+	return kp * error;
+}
+
+
+
+
+float pitch_controller::i_component(float input)
+{
+	return ki * summedError;
+}
+
+
+
+
+float pitch_controller::d_component(float input)
+{
+	return kd;
+}
--- Autopilot.h
+++ Autopilot.h
@@ -1,2 +1,115 @@
 #pragma once
 #include "Arduino.h"
+
+
+
+
+const float GRAVITY = 9.80665;
+
+
+
+
+float float_constrian(float input, float min, float max);
+
+
+
+
+class pitch_controller
+{
+public:
+	bool status = false;
+
+
+
+
+	void begin(float setpoint_,     // Degrees
+	           int maxRate_up_,     // Degrees per Sec
+	           int maxRate_down_,   // Degrees per Sec
+	           float kp_,           // Unitless
+	           float ki_,           // Unitless
+	           float kd_,           // Unitless
+	           float roll_comp_,    // Unitless
+	           float sampleRate_);  // Hz
+
+	float compute(float pitchAngle, // Degrees
+	              float rollAngle,  // Degrees
+	              float airspeed);  // m/s
+
+	float get_P_Component();
+	float get_I_Component();
+	float get_D_Component();
+
+
+
+
+private:
+	const float MAX_SETPOINT = 90.0;
+	const float MIN_SETPOINT = -90.0;
+
+	const float MAX_T = 1.0;
+	const float MIN_T = 0.4;
+
+	const float MAX_P = 3.0;
+	const float MIN_P = 0.1;
+
+	const float MAX_I = 0.5;
+	const float MIN_I = 0.0;
+
+	const float MAX_D = 0.2;
+	const float MIN_D = 0.0;
+
+	const int MAX_RATE_UP = 100;
+	const int MIN_RATE_UP = 0;
+
+	const int MAX_RATE_DN = 100;
+	const int MIN_RATE_DN = 0;
+
+	const float MAX_ROLL_COMP = 1.5;
+	const float MIN_ROLL_COMP = 0.7;
+
+	const int MAX_I_LIMIT = 4500;
+	const int MIN_I_LIMIT = 0;
+
+
+
+
+	unsigned long sampleTimer_previous;
+	unsigned long sampleTimer_current;
+	unsigned long sample_time_actual;
+
+	unsigned int samplePeriod_ms;
+	float samplePeriod_s;
+	float setpoint;
+	float kp;
+	float ki;
+	float kd;
+	float roll_comp;
+	int airspeed_scaler = 1;
+
+	float p_val;
+	float i_val;
+	float d_val;
+
+	int maxRate_up;
+	int maxRate_down;
+	float i_limit       = 1500; // Centi-Degrees
+	float error         = 0;
+	float previousError = 0;
+	float summedError   = 0;
+
+	float prevPitchAngle = 0;
+
+
+
+	
+	float find_kp(float kp_,
+	              float ki_,
+	              float kd_,
+	              float samplePeriod_s);
+	float find_ki(float ki_, float samplePeriod_s);
+	float omega();
+	float roll_compensation(float rollAngle, float airspeed);
+	float p_component(float input);
+	float i_component(float input);
+	float d_component(float input);
+};
